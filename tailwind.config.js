30fbfc8a39b365cda69e49401a1a9976ea29c9a0
slavko <slavkoj6@gmail.com>
2022-01-21 00:25:36 +0100

    landing page frontend first pass done

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: { "landing-image": "url('/assets/lohp_en_1302x955.png')" },
+    },
+  },
+  plugins: [],
+};
